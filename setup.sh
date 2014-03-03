c2d842c0739409654b112305e38ceb9bce0e3e32
Osman Surkatty <osman@surkatty.org>
2014-03-02 16:09:37 -0800

    Removed shred since it is installed by default and added full paths for each command.

--- setup.sh
+++ setup.sh
@@ -9,11 +9,11 @@
 # * Further testing
 
 if (( $EUID != 0 )); then
-  echo "This must be run as root. Type in 'sudo $0' to run it as root."
+  /bin/echo "This must be run as root. Type in 'sudo $0' to run it as root."
   exit 1
 fi
 
-cat <<'Onion_Pi'
+/bin/cat <<'Onion_Pi'
                             ~
                            /~
                      \  \ /**
@@ -41,23 +41,24 @@ cat <<'Onion_Pi'
                   /  /    \
 Onion_Pi
 
-echo "This script will auto-setup a Tor proxy for you. It is recommend that you
+/bin/echo "This script will auto-setup a Tor proxy for you. It is recommend that you
 run this script on a fresh installation of Raspbian."
 read -p "Press [Enter] key to begin.."
 
-echo "Updating package index.."
-apt-get update -y
+/bin/echo "Updating package index.."
+/usr/bin/apt-get update -y
 
-echo "Updating out-of-date packages.."
-apt-get upgrade -y
+/bin/echo "Updating out-of-date packages.."
+/usr/bin/apt-get upgrade -y
 
-echo "Downloading and installing various packages.."
-apt-get install -y tor chkrootkit unattended-upgrades ntp shred monit
+/bin/echo "Downloading and installing various packages.."
+/usr/bin/apt-get install -y tor chkrootkit unattended-upgrades ntp monit
 
-echo "Configuring Tor.."
-cat /dev/null > /etc/tor/torrc
+/bin/echo "Configuring Tor.."
+/bin/cat /dev/null > /etc/tor/torrc
 /etc/tor/torrc <<'onion_pi_configuration'
-# v0.2
+# Onion Pi Config v0.2
+# More information: https://github.com/breadtk/onion_pi/
 Log notice file /var/log/tor/notices.log
 VirtualAddrNetwork 10.192.0.0/10
 AutomapHostsSuffixes .onion,.exit
@@ -72,31 +73,31 @@ Exitpolicy reject *:*
 
 onion_pi_configuration
 
-echo "Fixing firewall configuration.."
-iptables -F
-iptables -t nat -F
-iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
-iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
-sh -c "iptables-save > /etc/iptables.ipv4.nat"
+/bin/echo "Fixing firewall configuration.."
+/sbin/iptables -F
+/sbin/iptables -t nat -F
+/sbin/iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
+/sbin/iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
+/bin/sh -c "/sbin/iptables-save > /etc/iptables.ipv4.nat"
 
-echo "Wiping various  files and directories.."
-shred -fvzu -n 3 /var/log/wtmp
-shred -fvzu -n 3 /var/log/lastlog
-shred -fvzu -n 3 /var/run/utmp
-shred -fvzu -n 3 /var/log/mail.*
-shred -fvzu -n 3 /var/log/syslog*
-shred -fvzu -n 3 /var/log/messages*
-shred -fvzu -n 3 /var/log/auth.log*
+/bin/echo "Wiping various  files and directories.."
+/usr/bin/shred -fvzu -n 3 /var/log/wtmp
+/usr/bin/shred -fvzu -n 3 /var/log/lastlog
+/usr/bin/shred -fvzu -n 3 /var/run/utmp
+/usr/bin/shred -fvzu -n 3 /var/log/mail.*
+/usr/bin/shred -fvzu -n 3 /var/log/syslog*
+/usr/bin/shred -fvzu -n 3 /var/log/messages*
+/usr/bin/shred -fvzu -n 3 /var/log/auth.log*
 
-echo "Setting up logging in /var/log/tor/notices.log.."
-touch /var/log/tor/notices.log
-chown debian-tor /var/log/tor/notices.log
-chmod 644 /var/log/tor/notices.log
+/bin/echo "Setting up logging in /var/log/tor/notices.log.."
+/usr/bin/touch /var/log/tor/notices.log
+/bin/chown debian-tor /var/log/tor/notices.log
+/bin/chmod 644 /var/log/tor/notices.log
 
-echo "Setting tor to start at boot.."
-update-rc.d tor enable
+/bin/echo "Setting tor to start at boot.."
+/usr/sbin/update-rc.d tor enable
 
-echo "Setting up Monit to watch Tor process.."
+/bin/echo "Setting up Monit to watch Tor process.."
 /etc/monit/monitrc << 'tor_monit'
 check process tor with pidfile /var/run/tor/tor.pid
 group tor
@@ -108,14 +109,14 @@ if failed port 9050 type tcp
 if 3 restarts within 5 cycles then timeout
 tor_monit
 
-echo "Starting monit.."
-monit quit
-monit -c /etc/monit/monitrc
+/bin/echo "Starting monit.."
+/usrb/bin/monit quit
+/usrb/bin/monit -c /etc/monit/monitrc
 
-echo "Starting tor.."
-service tor start
+/bin/echo "Starting tor.."
+/usr/sbin/service tor start
 
-echo "Setup complete!
+/bin/echo "Setup complete!
 To connect to your own node set your web browser to connect to:
   Proxy type: SOCKSv5
   IP: $(hostname -i | awk '{print $1}')
